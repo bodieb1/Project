1475c109dde91591e67265ae961499d49da9c386
Bodie Brewer <unconfigured@null.spigotmc.org>
2018-01-22 21:13:50 -0500

    Work on Crash Report

--- src/com/mangodev/ICrashReportDetail.java
+++
@@ -1,7 +0,0 @@
-package com.mangodev;
-
-import java.util.concurrent.Callable;
-
-public interface ICrashReportDetail<V> extends Callable<V>{
-	
-}
--- src/com/mangodev/Reference.java
+++ src/com/mangodev/Reference.java
@@ -6,19 +6,7 @@ import java.util.Date;
 
 public class Reference {
 	
-	public static String timeStamp = new SimpleDateFormat("HH:mm:ss").format(new Date());
 	
-	public static void Stamp(String messageType, String info) throws IllegalArgumentException {
-		if(messageType == "ERROR"){
-				System.err.println("[" + timeStamp + "][SYSTEM/" + messageType + "] " + info + "\n");
-		} else if(messageType == "INFO") {
-			System.out.println("[" + timeStamp + "][SYSTEM/" + messageType + "] " + info + "\n");
-		} else {
-			Throwable throwable = new Throwable();
-			CrashReport crashreport = CrashReport.makeCrashReport(throwable, "Invalid Console Entry");
-            CrashReportCategory crashreportcategory = crashreport.makeCategory("Method With Error");
-            crashreportcategory.addCrashSection("Method", new Exception().getStackTrace()[0].getMethodName());
-            throw new ReportedException(crashreport);
-			}		
-	}	
+	
+	
 }
